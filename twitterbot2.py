b032ad0a103ba9fbeeb43b1dda2b51520dc524e3
saarusudhan <69858775+saarusudhan@users.noreply.github.com>
2020-08-26 18:07:01 +0530

    Create twitterbot2.py

---
+++ twitterbot2.py
@@ -0,0 +1,37 @@
+import tweepy
+import os # operating system library
+
+def create_api():
+  consumer_key = 'JxtXWjMsS9pkflxoW1ThdGTY1'
+  consumer_secret = '9vwPS7PXg1qYbz7dkT9HK6zwX7zviRebpZWqdZx5Qns4K1Av0v'
+  access_token = '1298267848586928129-t2Pmdr1yr6ndoNOhDGNEeCoDZygsT5'
+  access_token_secret = 'kWsczmjaVhJkZyqj36TYmaVfN6AlDiR4nic8aHHb23rmr'
+
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_token_secret)
+
+  api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+  api.verify_credentials()
+  print('API Created')
+  return api
+  
+# Complete code
+import time
+
+def follower_count(user):
+  emoji_numbers =  {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
+                      4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣", 8: "8️⃣", 9: "9️⃣"}
+
+  uf_split = [int(i) for i in str(user.followers_count)]# Used to seperate 
+
+  emoji_followers = ''.join([emoji_numbers[j] for j in uf_split if j in emoji_numbers.keys()]) 
+  return emoji_followers
+
+api = create_api()
+
+while True:
+    user = api.get_user('@SudhanSaaru')
+    api.update_profile(name=f'SAARU_SUDHAN|{follower_count(user)} Followers')
+    print(f'Updating Twitter Name : SAARU_SUDHAN|{follower_count(user)} Followers')
+    print('Waiting to refresh')
+    time.sleep(60)
