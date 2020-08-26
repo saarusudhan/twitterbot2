8ee93ed96b8f97a968e22bb64a14d886771ae69a
saarusudhan <69858775+saarusudhan@users.noreply.github.com>
2020-08-26 19:52:52 +0530

    Update twitterbot2.py

--- twitterbot2.py
+++ twitterbot2.py
@@ -35,6 +35,6 @@ while True:
     print(f'Updating Twitter Name : SAARU_SUDHAN|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
-      
+       
        
              
